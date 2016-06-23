988d5996ecdbadd5a947afb17ded2e79d648ce19
jbok4 <jbok4@aol.com>
2016-06-23 16:56:51 -0400

    models added

---
+++ app/models/Cyclist.rb
@@ -0,0 +1,14 @@
+class Cyclist < ActiveRecord::Base
+  has_many :rides
+  validates_presence_of :username, :email, :password
+  has_secure_password
+
+  def slug
+    username.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
+end
